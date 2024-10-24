a83a487874aa05a0709d747446c93d4272fed816
Gabriel Cepeda <gabrielcepeda8@gmail.com>
2024-10-23 21:37:04 -0400

    dbt with airflow

--- dags/extract_info_banks_stocks.py
+++ dags/extract_info_banks_stocks.py
@@ -1,46 +1,38 @@
 from datetime import datetime
-from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.decorators import task
 from extract_info_banks_stocks_tasks.scraping_banks_info import extract_bank_and_financial_services_stocks
-from extract_info_banks_stocks_tasks.save_banks_info_db import save_bank_stocks_to_db
-# from extract_info_banks_stocks_tasks.scraping_banks_stocks import get_banks_stocks
-# from extract_info_banks_stocks_tasks.save_stocks_info import save_stocks_info_to_db
 from extract_info_banks_stocks_tasks.extract_save_basic_info import get_basic_info
 from extract_info_banks_stocks_tasks.extract_save_fundamentals_info import get_fundamentals_info
 from extract_info_banks_stocks_tasks.extract_save_price_stock import get_price_info
 from extract_info_banks_stocks_tasks.extract_save_holders_info import get_holders_info
 from extract_info_banks_stocks_tasks.extract_save_calificadores_info import get_calificadores_info
 import os
-from airflow import DAG
-from airflow.utils.dates import days_ago
+from airflow.decorators import dag
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-from airflow.operators.bash import BashOperator
-
 from cosmos import DbtDag, ProjectConfig, ProfileConfig
-from cosmos.operators import DbtDocsOperator
-from cosmos.config import RenderConfig
 from cosmos.profiles import ClickhouseUserPasswordProfileMapping
 
-
-
+# Obtener las variables de entorno
+password_ch = os.getenv("CLICKHOUSE_PASSWORD", "default") 
+port_ch = int(os.getenv("CLICKHOUSE_TCP_PORT", 9000))
 host = os.getenv("DB_HOST", "postgres-db")         
 database = os.getenv("POSTGRES_DB", "landing_zone")      
 user = os.getenv("POSTGRES_USER", "sib_user")        
-password = os.getenv("POSTGRES_PASSWORD", "sib_user") 
+password_pg = os.getenv("POSTGRES_PASSWORD", "sib_user") 
 port = os.getenv("POSTGRES_PORT", 5432)        
 AIRBYTE_CONN_ID=os.getenv("AIRBYTE_CONNECTION_ID", "8bf4a1bc-7071-4b67-8d55-4b5be1484508")
-ENGINE_DB = f'postgresql://{user}:{password}@{host}:{port}/{database}'
+ENGINE_DB = f'postgresql://{user}:{password_pg}@{host}:{port}/{database}'
 
 # Definir el DAG
-with DAG(
+@dag(
     'extract_info_banks_stocks',
     description='DAG para buscar la información de los stocks de los bancos que cotizan en la bolsa de USA',
     schedule_interval=None,
     start_date=datetime(2024, 10, 1),
     catchup=False,
-) as dag:
+)
+def extract_info_banks_stocks():
 
     # Tarea 1: Extraer información de los bancos
     extract_banks_task = PythonOperator(
@@ -117,16 +109,11 @@ with DAG(
         wait_seconds=3
     )
 
-    # Tarea que ejecuta dbt test para testear los modelos en el stage
-    dbt_test = BashOperator(
-        task_id='dbt_test',
-        bash_command='dbt test --profiles-dir /home/airflow/.dbt --project-dir /opt/airflow',
-    )
-
-    # Tarea que ejecuta dbt run para ejecutar los modelos en clickhouse
-    dbt_run = BashOperator(
-        task_id='dbt_run',
-        bash_command='dbt run --profiles-dir /home/airflow/.dbt --project-dir /opt/airflow',
+    trigger_execute_dbt_jobs = TriggerDagRunOperator(
+        task_id='trigger_execute_dbt_jobs',
+        trigger_dag_id='execute_dbt_jobs',
+        trigger_rule='all_success',
+        wait_for_completion=True,
     )
 
     extract_banks_task >> [
@@ -135,4 +122,36 @@ with DAG(
                         extract_save_price_stocks_info,
                         extract_save_holders_info,
                         extract_save_calificadores_info
-    ] >> send_to_datawarehouse >> dbt_test >> dbt_run
+    ] >> send_to_datawarehouse >> trigger_execute_dbt_jobs
+
+extract = extract_info_banks_stocks()
+
+execute_dbt_jobs = DbtDag(
+    dag_id="execute_dbt_jobs",
+    project_config=ProjectConfig(
+        "/home/dbt"
+    ),
+    profile_config=ProfileConfig(
+        profile_name="dbt_project",
+        target_name="dev",
+        profile_mapping=ClickhouseUserPasswordProfileMapping(
+            conn_id="clickhouse_conn_id",
+            profile_args={
+                "host": "clickhouse",
+                "user": "default",
+                "port": port_ch,
+                "password" : password_ch,
+                "clickhouse": True
+            }
+        ),
+    ),
+    operator_args={
+        "install_deps": True,  
+        "full_refresh": True, 
+    },
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    default_args={"retries": 2},
+)
+
+execute_dbt_jobs
